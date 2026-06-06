60e49e8e81f16f1a78194f393d7893dccf926f7f
Veledaa <daviveleda@Outlook.com.br>
2026-06-06 03:51:17 -0300

    Adicionado melhoria nas informações do perfil (nome, nick, id, data criação, data entrada, atitivade) além da criação do comando /avatar para verificar fotos de usuários

--- comandos/perfil.py
+++ comandos/perfil.py
@@ -16,15 +16,85 @@ class Perfil(commands.Cog):
         if usuario is None:
             usuario = interaction.user
 
+
         
         embed = discord.Embed(
-            title=f"Perfil de {usuario.name}"
+            title=f"Perfil de {usuario.name}",
+            color= usuario.color
+        )
+
+
+        embed.set_thumbnail(
+            url = usuario.display_avatar.url
+        )        
+
+
+        embed.add_field(
+            name= "Nome",
+            value= usuario.name,
+            inline= False
+        )
+
+
+        embed.add_field(
+            name= "Nick",
+            value= usuario.display_name,
+            inline= False
+        )
+
+
+        embed.add_field(
+            name= "ID",
+            value= usuario.id,
+            inline= False
+        )
+
+
+        embed.add_field(
+            name= "Criação da Conta",
+            value= usuario.created_at.strftime("%d/%m/%Y"),
+            inline= True
+        )
+
+
+
+        embed.add_field(
+            name= "Data Entrada Servidor",
+            value= usuario.joined_at.strftime("%d/%m/%Y"),
+            inline= True
+        )
+
+
+        embed.add_field(
+            name= "Atividade",
+            value= usuario.activity,
+            inline= False
+        )
+
+        
+
+        await interaction.response.send_message(
+            embed=embed,
+        )
+
+
+
+    @app_commands.command(name="avatar", description="Verificar o Avatar (foto) do usuário.")
+    async def avatar(self, interaction:discord.Interaction, usuario:discord.Member = None):
+
+        if usuario is None:
+            usuario = interaction.user
+
+
+        embed = discord.Embed(
+            title= f"Avatar de {usuario.name}",
+            color= usuario.color
         )
 
 
         embed.set_image(
-            url = usuario.display_avatar.url
-        )        
+            url= usuario.display_avatar.url
+        )
 
 
         await interaction.response.send_message(
