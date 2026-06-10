62485adf453545bccf97da14faf1fdb3930ef9ac
Veledaa <daviveleda@Outlook.com.br>
2026-06-10 10:50:00 -0300

    Adicionado comando /buildLol. implementado Scraping das botas do OP.gg

--- comandos/lol.py
+++ comandos/lol.py
@@ -4,6 +4,7 @@ import httpx
 
 from discord import app_commands
 from discord.ext import commands
+from bs4 import BeautifulSoup
 
 
 
@@ -14,11 +15,81 @@ class LeagueOfLegends(commands.Cog):
 
 
     @app_commands.command(name="lolbuild", description="Verificar a runa e build de um campeão.")
-    async def BuildLol(self, interaction:discord.Interaction, nomepersonagem:str):
+    async def BuildLol(self, interaction:discord.Interaction, champion:str):
 
-        url = ""
+        await interaction.response.defer()
 
-   
+
+        url = f"https://op.gg/lol/champions/{champion.lower()}/build"
+
+        headers = {
+            "User-Agent": (
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                        "AppleWebKit/537.36 (KHTML, like Gecko) "
+                        "Chrome/137.0.0.0 Safari/537.36"
+                    )
+        }
+
+
+        async with httpx.AsyncClient() as client:
+
+            resposta = await client.get(url, headers=headers)
+
+            if resposta.status_code != 200:
+                await interaction.followup.send(
+                    "Não foi possivél obter os itens."
+                )
+
+                return
+
+            html = resposta.text
+
+            
+
+        soup = BeautifulSoup(html, "html.parser")
+            
+        tabelas = soup.find_all("table")
+
+        bota = "Não encontrada"
+        pick_rate = "?"
+        win_rate = "?"
+
+        for tabela in tabelas:
+
+            caption = tabela.find("caption")
+
+            if caption and caption.text.strip() == "Boots Table":
+
+                primeira_linha = tabela.find("tbody").find("tr")
+
+                bota = primeira_linha.find("img")["alt"]
+
+                stats = primeira_linha.find_all("strong")
+
+                pick_rate = stats[0].text
+                win_rate = stats[1].text
+
+                break
+
+        
+        
+        embed = discord.Embed(
+            title=f"Build {champion}",
+            description="Verificar itens da build:",
+        )
+
+        embed.add_field(
+            name="👢 Bota mais utilizada: \n",
+            value=(
+                f"**{bota}**"
+            ),
+            inline=False
+        )
+
+        await interaction.followup.send(
+            embed=embed
+        )
+        
 
 async def setup(bot):
     await bot.add_cog(LeagueOfLegends(bot))
